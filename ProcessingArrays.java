87266602dc60b87b07a8dd5cb8f2db638ac12bb1
cclin <kuanyu0111@gmail.com>
2018-05-24 10:00:41 -0700

    practice for array

---
+++ ProcessingArrays.java
@@ -0,0 +1,45 @@
+public class ProcessingArrays
+{
+public static void main(String[] args )
+ {
+   int[] arr = {78, 498, 44, 56, 34, 45, 23};
+   
+   System.out.println("First way of printing an array");
+   
+   for (int i=0; i < arr.length; i++)
+    {  
+    System.out.println(arr[i]);
+    }
+    System.out.println("Second way");
+    for (int elem : arr)
+    {
+      System.out.println(elem);
+    }
+
+    System.out.print("The minimum value of the array is: ");
+    System.out.println(minValue(arr));
+    System.out.print("The index of the minimun value is: ");
+    System.out.println(minIndex(arr));
+ }
+static int minValue (int[] arr)
+{
+  int min = arr[0];
+
+  for (int elem : arr)
+    if (elem < min)
+      min = elem;
+  return min;
+}
+static int minIndex (int[] arr)
+{
+  int index = 0, min = arr[0];
+  for (int i = 1; i < arr.length; i ++)
+  {
+    if (arr[i] < min )
+    {
+      min = arr[i];
+      index = i;
+    }
+  }
+  return index;
+}}
