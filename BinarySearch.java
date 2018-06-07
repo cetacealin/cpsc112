47e504c1f79b61165077686a789fde1b30d7ea40
cclin <kuanyu0111@gmail.com>
2018-06-07 09:47:20 -0700

    two methods of binary search

---
+++ BinarySearch.java
@@ -0,0 +1,42 @@
+public class BinarySearch
+{
+  public static void main(String[] args)
+  {
+    int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int f = 0, l = 14, t = 8;
+    System.out.println(binarySearchRecursive(arr, f, l, t));
+    System.out.println(binarySearch(arr, t));
+
+  }
+  public static int binarySearchRecursive (int[] arr, int first, int last,int target)
+  {
+    if (first > last)
+      return -1;
+
+    int mid = (first + last) / 2;
+
+    if (target == arr[mid])
+      return mid;
+    if (target > arr[mid])
+      first = mid + 1;
+    else last = mid - 1;
+    
+    return binarySearchRecursive(arr, first, last, target);
+  }
+  public static int binarySearch(int[] arr, int target)
+  {
+    int first = 0, last = arr.length - 1;
+
+    while (first <= last)
+    {
+      int mid = (first + last) / 2;
+      if (target == arr[mid])
+        return mid;
+      if (target > arr[mid])
+        first = mid + 1;
+      else
+        last = mid -1;
+    }
+    return -1;
+}
+}
