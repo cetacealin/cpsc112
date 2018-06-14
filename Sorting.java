0d4cdaff1cc1d6606de0fbc696d6f6941ce25554
cclin <kuanyu0111@gmail.com>
2018-06-14 10:43:37 -0700

    merge sort

--- Sorting.java
+++ Sorting.java
@@ -5,7 +5,8 @@ public class Sorting
   {
     int[] arr = {6, 3, 7, 2, 8, 99, 4};
     int[] arr2 = {99, 456, 23, 64, 835, 245};
-    int[] arr3 = {3, 5, 342, 12, 63, 3422, 899};
+    int[] arr3 = {5, 3, 42, 12, 63, 3422, 899};
+    int[] arr4 = {24, 61, 6456, 33332, 43, 3, 2};
     System.out.println("The original array is: " + Arrays.toString(arr));
     selectionSort(arr);
     System.out.println("After using selectionSort method: " + Arrays.toString(arr));
@@ -18,15 +19,12 @@ public class Sorting
     bubbleSort(arr3);
     System.out.println("After using bubbleSort method: " + Arrays.toString(arr3));
     System.out.println();
-    int x = 1, y  = 1, z = 0, a = (x + y + z)/3;
-    System.out.println(a);
-
-
-
-
+    System.out.println("The array before mergeSort: " + Arrays.toString(arr4));
+    mergeSort(arr4, 0, arr4.length - 1);
+    System.out.println("After mergeSort method: " + Arrays.toString(arr4));
   }
   public static void selectionSort (int[] data)
-    //sorting in place (no extra array, use single memory locaion to swap)
+    //sorting in (same) place (no extra array, use single memory locaion to swap)
   {
     for (int i = 0; i < data.length - 1; i++)
     {
@@ -44,6 +42,8 @@ public class Sorting
     int temp = data[i];
     data[i] = data[j];
     data[j] = temp;
+    System.out.println(Arrays.toString(data));
+
   }
   public static void insertionSort (int[] data)
   {
@@ -65,12 +65,43 @@ public class Sorting
   {
     for ( int i = data.length - 1; i > 0; i--)
     {
+ 
       for (int j = 0; j <= i - 1; j++)
       {
         if (data[j] > data[j + 1])
           swap(data, j, j + 1);
       }
     }
+  }
+  public static void mergeSort (int[] data, int first, int last)
+  {
+    if (first >= last)
+      return;
+    int mid =  (first + last) / 2;
+    mergeSort(data, first, mid);
+    mergeSort(data, mid + 1, last);
+
+    merge (data, first, last);
+  }
+  public static void merge(int[] data, int first, int last)
+  {
+    int[] temp = new int[last - first + 1];
+      int mid = (first + last) / 2;
+    int i = first, j = mid + 1, k = 0;
+
+    while (i <= mid && j <= last)
+    {
+      if (data[i] < data[j])
+        temp[k++] = data[i++];
+      else
+        temp[k++] = data[j++];
+    }
+    while (i <= mid)
+      temp[k++] = data[i++];
+    while (j <= last)
+      temp[k++] = data[j++];
+    for (i = first; i <= last; i++)
+      data[i] = temp[i - first];
   }
 }
 
